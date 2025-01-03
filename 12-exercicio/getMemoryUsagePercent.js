68f14f09e5b395292e5490d8acb5ba6106bb2021
Kaue-Dev <kauemassola900@gmail.com>
2025-01-03 16:47:47 -0300

    feat: function to get the memory usage percent

---
+++ 12-exercicio/getMemoryUsagePercent.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+function getMemoryUsagePercent() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  return ((usedMemory / totalMemory) * 100).toFixed(2);
+}
+
+module.exports = getMemoryUsagePercent;
